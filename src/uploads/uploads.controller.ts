bb454350c6ecfba5302084b09a2f5c2ed0a77f60
Bohdan Shyiatyi <koder2563@gmail.com>
2024-01-17 16:25:35 +0100

    get image from server

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,25 +1,28 @@
-import {
-    Body,
-    Controller,
-    Delete,
-    Post,
-    UploadedFile,
-    UseInterceptors,
-  } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadsService } from './uploads.service';
 import { KeyFileDto } from './dto/key-file-dto';
+import { Response } from 'express';
   
 @Controller()
 export class UploadController {
 
     constructor(private readonly uploadService: UploadsService) {}
 
-    @Post('upload')
+    @Post('upload/:directory')
     @UseInterceptors(FileInterceptor('file'))
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
+    uploadFile(@UploadedFile() file: Express.Multer.File, @Param('directory') directory: string) {
         try {
-            return this.uploadService.uploadFile(file);
+            return this.uploadService.uploadImage(file, directory);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    @Get('upload/:folder/:image')
+    getImage(@Param('folder') folder: string, @Param('image') image: string, @Res() res: Response) {
+        try {
+            return this.uploadService.getImage(folder, image, res)
         } catch (error) {
             throw error;
         }
--- src/uploads/uploads.service.ts
+++ src/uploads/uploads.service.ts
@@ -1,6 +1,9 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, DeleteObjectCommand, HeadObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { Upload } from 'express-fileupload';
+import * as mimeTypes from 'mime-types';
+import { Response } from 'express';
+import axios from 'axios';
 
 @Injectable()
 export class UploadsService {
@@ -14,25 +17,28 @@ export class UploadsService {
         },
     });
 
-    async uploadFile(file: Upload) {
+    async uploadImage(file: Upload, directory: string) {
+
         if (!file || !file.buffer) {
             throw new BadRequestException('No file data found.');
         }
+        
+        const mimeType = mimeTypes.lookup(file.originalname);
+        
+        if (!mimeType || !mimeType.startsWith('image/')) {
+            throw new BadRequestException('Invalid file format. Only images are allowed.');
+        }
     
-        const { originalname } = file;
-
-        return await this.s3_upload(
-            file.buffer,
-            this.AWS_S3_BUCKET,
-            originalname,
-            file.mimetype,
-        );
+        return await this.s3_upload(file.buffer, this.AWS_S3_BUCKET, file.mimetype, directory);
     }
 
-    private async s3_upload(file: Buffer, bucket: string, name: string, mimetype: string) {
+    private async s3_upload(file: Buffer, bucket: string, mimetype: string, directory: string) {
+        const timestamp = Date.now();
+        const keyFile = `image_${timestamp}.jpg`;
+
         const params = {
             Bucket: bucket,
-            Key: String(name),
+            Key: `${directory}/${String(keyFile)}`,
             Body: file,
             ContentType: mimetype,
             ContentDisposition: 'inline',
@@ -42,27 +48,38 @@ export class UploadsService {
         };
 
         try {
-            const s3Response = await this.s3.send(new PutObjectCommand(params));
-            return s3Response;
+            await this.s3.send(new PutObjectCommand(params));
+            return { image: params.Key };
         } catch (e) {
             throw e;
         }
     }
 
+    async getImage(folder: string, image: string, res: Response) {
+        const s3Url = `https://${this.AWS_S3_BUCKET}.s3.${this.LocationConstraint}.amazonaws.com/${folder}/${image}`;
+    
+        const response = await axios.get(s3Url, { responseType: 'arraybuffer' });
+        const contentType = response.headers['content-type'];
+    
+        res.set('Content-Type', contentType);
+    
+        const contentLength = response.headers['content-length'];
+
+        if (contentLength) {
+          res.set('Content-Length', contentLength);
+        }
+    
+        res.send(response.data);
+    }
+
     async deleteFile(key: string) {
-        const params = {
-            Bucket: this.AWS_S3_BUCKET,
-            Key: key,
-        };
+        const params = { Bucket: this.AWS_S3_BUCKET, Key: key };
 
         try {
             const s3Response = await this.s3.send(new DeleteObjectCommand(params));
             return s3Response;
-        } catch (e) {
-            if (e.name === 'NoSuchKey') {
-                throw new NotFoundException('File not found.');
-            }
-            throw e;
+        } catch (error) {
+            throw error;
         }
     }
 }
